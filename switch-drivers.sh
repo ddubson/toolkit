9ffc4c6240e4d10f217418749728a65b162dba58
Dmitriy Dubson <ddubson@vmware.com>
2020-08-26 17:35:06 -0400

    Add switch-drivers script

---
+++ switch-drivers.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# NOTE: Script uses a MacOS specific clipboard command. MacOS and pbcopy command are required.
+
+current_git_repo=$(pwd)
+
+pushd "$current_git_repo" > /dev/null || exit
+  pushd "$(git rev-parse --show-toplevel)" > /dev/null || exit
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  echo "🌿 Current branch: $current_branch"
+  if [[ $current_branch == 'main' || $current_branch == 'master' || $current_branch == 'dev' ]]; then
+      echo "Switching drivers requires creating a WIP commit, you can't be on main, master, oxr dev"
+      echo "Exiting quietly."
+      exit 1
+  fi
+
+  git add .
+  git commit -m "WIP: Switching drivers"
+
+  echo "Pushing to git remote!"
+  git push -u origin head 2&> /dev/null
+  echo "git checkout $current_branch && git pull origin $current_branch --rebase" | pbcopy
+  popd > /dev/null || exit
+popd > /dev/null || exit
+
+echo "
+-----------------------------------------------------------------------------
+✅ Ready to switch drivers! 🚗 Command to switch drivers copied to clipboard
+-----------------------------------------------------------------------------
+"
